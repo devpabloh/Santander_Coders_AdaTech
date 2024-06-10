18f0ac71bd23d5b1cb5aca20bebc2eb7207873c6
Dev. Pablo H. <devpabloh@gmail.com>
2024-06-09 23:38:16 -0300

    Update aula_01.js

--- JS/aula_01.js
+++ JS/aula_01.js
@@ -1,5 +1,12 @@
-console.log("Hello Word!")
+
 
 var nome = "Pablo Henrique" //string
 var numero = 27 //number
 var VerdadeOuMentira = true //booleano (recebe true ou false)
+
+let nota1 = 10
+let nota2 = 8
+
+let soma = nota1 + nota2
+
+console.log(soma)
