a141f67d29f3aebf89046f31c922bdd25be06c88
Dev. Pablo H. <devpabloh@gmail.com>
2024-06-10 09:30:46 -0300

    atualização

--- JS/condicionais2.js
+++ JS/condicionais2.js
@@ -1,8 +1,17 @@
 // Switch / case é muito bom quando sua variável possui valores específicos.
 
-const permissões = 'Aluno' // pode receber "aluno" || "professor" || "administrador"
+const permissoes = "administrador" // pode receber "aluno" || "professor" || "administrador"
 
-switch(variavel){
-    case
-    break
+switch(permissoes){
+    case "aluno":
+        console.log("Você só pode visualizar as aulas e exercícios")
+        break
+    case "professor":
+        console.log("você pode visualizar, alterar e adicionar as aulas e exercícos.")
+        break
+    case "administrador":
+        console.log("Você pode tudo meu consagrado, inclusive, se quiser passar na sala da TI para tomar um café, sinta-se a vontade.")
+        break
+    default:
+        console.log("Rapaz, quem é você ?")
 }
