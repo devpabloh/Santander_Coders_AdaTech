459ff80a9b0bd8328e31d55e0489d30f5f67e02f
Dev. Pablo H. <devpabloh@gmail.com>
2024-06-10 09:20:57 -0300

    atualização

--- JS/condicionais.js
+++ JS/condicionais.js
@@ -1,6 +1,6 @@
 const prompt = require('readline-sync')
 
-const idade = Number(prompt.question("Qual e a sua idade?"))
+/* const idade = Number(prompt.question("Qual e a sua idade?"))
 
 const ehMaiorDeIdade = idade >= 18; 
 
@@ -9,4 +9,14 @@ if(ehMaiorDeIdade){
     console.log("Você e maior de idade")
 }else{
     console.log("você é menor de idade")
+} */
+
+media = 4
+
+if(media >= 7){
+    console.log("Você foi aprovado")
+}else if(media < 7 & media >=5){
+    console.log("Você está na recuperação")
+}else{
+    console.log("Você está reprovado")
 }
---
+++ JS/condicionais2.js
@@ -0,0 +1,8 @@
+// Switch / case é muito bom quando sua variável possui valores específicos.
+
+const permissões = 'Aluno' // pode receber "aluno" || "professor" || "administrador"
+
+switch(variavel){
+    case
+    break
+}
