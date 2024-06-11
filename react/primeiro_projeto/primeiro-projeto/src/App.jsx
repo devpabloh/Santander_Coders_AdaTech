1ba70dfc169bce092dfda00f46438f4fd2238ad0
Dev. Pablo H. <devpabloh@gmail.com>
2024-06-11 18:07:45 -0300

    atualização

--- react/primeiro_projeto/primeiro-projeto/src/App.jsx
+++ react/primeiro_projeto/primeiro-projeto/src/App.jsx
@@ -1,28 +1,16 @@
 import React from "react";
-import foguete from "./assets/images/foguete.png"
 import "./style/app.css"
+import Navbar from "./components/Navbar/Navbar";
+
+
 
 /* Componente em classe é uma classe que herda a classe Component do React e retorna HTML dentro do método render */
 
 class App extends React.Component {
-
-
   render(){ // método responsável por renderizar o conteúdo HTML do nosso componente render(){}
+
     return (
-      <header>
-        <nav id="navbar">
-          <div className="nav-brand">
-            <img src={foguete} alt="" />
-            <h1>Space Flight News</h1>
-          </div>
-          <ul className="nav-list">
-            <li><a href="/">Home</a></li>
-            <li><a href="/">Trending</a></li>
-            <li><a href="/">Categories</a></li>
-            <li><a href="/">About us</a></li>
-          </ul>
-        </nav>
-      </header>
+      <Navbar />
     )
   }
 }
