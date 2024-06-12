00eefca65c9a9bc84d8360781cac04254b1b4218
Dev. Pablo H. <devpabloh@gmail.com>
2024-06-11 23:15:30 -0300

    Create Article.jsx

---
+++ react/primeiro_projeto/primeiro-projeto/src/components/Article/Article.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export class Article extends React.Component{
+    render(){
+        return (
+            <article id="article">
+                <img src="" alt="" />
+
+                <div className="article-infos">
+                    <h2>Designing Dashboards</h2>
+                    <h2>Nasa</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam, tempore recusandae quisquam qui dicta, sapiente at repellat veritatis cumque quos ab dignissimos maiores dolores porro quidem, sint eos impedit quasi.</p>
+                </div>
+            </article>
+        )
+    }
+
+}
+
